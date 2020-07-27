792dd87847d2810dc36ab92d0cfb11a15e80596f
cristovan-acs <cristovan.acs@gmail.com>
2020-07-27 07:02:09 -0300

    Criando comida

--- script.js
+++ script.js
@@ -7,18 +7,28 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+
 let direction = "right";            
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG() {
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
+
 function criarCobrinha(){
     for(i=0; i < snake.length; i++){
     context.fillStyle = "blue";
     context.fillRect(snake[i].x, snake[i].y, box, box);
-
     }
+}
+
+function drawnFood(){
+    context.fillStyle = "red"
+    context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener("keydown", update);
@@ -39,6 +49,7 @@ function iniciarJogo(){
 
     criarBG();
     criarCobrinha();
+    drawnFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
@@ -56,4 +67,5 @@ function iniciarJogo(){
     }
     snake.unshift(newHead);
 }
+
 let jogo = setInterval(iniciarJogo, 100);
