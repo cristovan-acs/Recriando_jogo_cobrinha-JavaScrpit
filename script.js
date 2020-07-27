cfa058a031d9bbaa84e0de4774fa35d86b708641
cristovan-acs <cristovan.acs@gmail.com>
2020-07-27 06:34:15 -0300

    Desenvolvendo a cobrinha

--- script.js
+++ script.js
@@ -3,8 +3,20 @@ let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
 
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 function criarBG() {
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
+function criarCobrinha(){
+    for(i=0; i < snake.length; i++){
+    context.fillStyle = "blue";
+    context.fillRect(snake[i].x, snake[i].y, box - 1, box - 1);
+
+    }
+}
 criarBG();
+criarCobrinha();
